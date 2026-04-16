64b8555c10e5e73289cc62aadbac04257370ca4b
hoaiphuc24012004 <hoaiphuc24012004@gmail.com>
2026-04-16 13:50:25 +0700

    factor(doamin): Create DocumentSeuqence to generate sequence code

---
+++ src/SupplyCoreERP.Domain/DocumentSequences/DocumentSequence.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using Volo.Abp.Domain.Entities;
+
+namespace SupplyCoreERP.DocumentSequences
+{
+    public class DocumentSequence : AggregateRoot<Guid>
+    {
+        public string DocumentType { get; set; }
+        public string PrefixDate { get; set; }
+        public int LastValue { get; set; }
+
+        protected DocumentSequence() { }
+
+        public DocumentSequence(Guid id, string documentType, string prefixDate) : base(id)
+        {
+            DocumentType = documentType;
+            PrefixDate = prefixDate;
+            LastValue = 1;
+        }
+
+        public void Increment(string today)
+        {
+            if (PrefixDate != today)
+            {
+                PrefixDate = today;
+                LastValue = 1;
+            }
+            else
+            {
+                LastValue += 1;
+            }
+        }
+
+    }
+}
