fdad5531a963ca1670185c9e88da1d75cf559159
hoaiphuc24012004 <hoaiphuc24012004@gmail.com>
2026-04-17 13:19:01 +0700

    Deploy Angular

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,35 +1,30 @@
 import { Environment } from '@abp/ng.core';
 
-const baseUrl = 'http://localhost:4200';
-
-const oAuthConfig = {
-  issuer: 'https://localhost:44367/',
-  redirectUri: baseUrl,
-  clientId: 'SupplyCoreERP_App',
-  responseType: 'code',
-  scope: 'offline_access SupplyCoreERP',
-  requireHttps: true,
-};
+const baseUrl = 'https://supply-core-erp.vercel.app'; 
+const backendUrl = 'https://rxlogistics.up.railway.app/';
 
 export const environment = {
   production: true,
   application: {
     baseUrl,
-    name: 'SupplyCoreERP',
+    name: 'RxLogistics',
   },
-  oAuthConfig,
+  oAuthConfig: {
+    issuer: backendUrl + '/',
+    redirectUri: baseUrl,
+    clientId: 'SupplyCoreERP_App',
+    responseType: 'code',
+    scope: 'offline_access SupplyCoreERP',
+    requireHttps: true,
+  },
   apis: {
     default: {
-      url: 'https://localhost:44367',
+      url: backendUrl,
       rootNamespace: 'SupplyCoreERP',
     },
     AbpAccountPublic: {
-      url: oAuthConfig.issuer,
+      url: backendUrl + '/',
       rootNamespace: 'AbpAccountPublic',
     },
   },
-  remoteEnv: {
-    url: '/getEnvConfig',
-    mergeStrategy: 'deepmerge'
-  }
 } as Environment;
