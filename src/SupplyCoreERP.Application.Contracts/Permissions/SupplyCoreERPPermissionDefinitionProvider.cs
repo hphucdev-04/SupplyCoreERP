1c8aacf48c3a6be99dfb5725ac79f6a671261ed3
hoaiphuc24012004 <hoaiphuc24012004@gmail.com>
2026-04-18 10:53:44 +0700

    config(perimission): Config perrmission

--- src/SupplyCoreERP.Application.Contracts/Permissions/SupplyCoreERPPermissionDefinitionProvider.cs
+++ src/SupplyCoreERP.Application.Contracts/Permissions/SupplyCoreERPPermissionDefinitionProvider.cs
@@ -8,8 +8,9 @@ public class SupplyCoreERPPermissionDefinitionProvider : PermissionDefinitionPro
 {
 	public override void Define(IPermissionDefinitionContext context)
 	{
-		//Tạo Group chính: Catalog
-		var catalogGroup = context.AddGroup(SupplyCoreERPPermissions.Catalog.GroupNameCatalog, L("Permission:Catalog"));
+        #region Catalog Permission
+        // Parent Group
+        var catalogGroup = context.AddGroup(SupplyCoreERPPermissions.Catalog.GroupNameCatalog, L("Permission:Catalog"));
 
 		//Category
 		AddCrudPermissions(catalogGroup, SupplyCoreERPPermissions.Catalog.Category.Default, "Category");
@@ -27,39 +28,125 @@ public class SupplyCoreERPPermissionDefinitionProvider : PermissionDefinitionPro
 		AddCrudPermissions(catalogGroup, SupplyCoreERPPermissions.Catalog.Manufacturer.Default, "Manufacturer");
 
 		//Medicine 
-		var medicinePerm = AddCrudPermissions(catalogGroup, SupplyCoreERPPermissions.Catalog.Medicine.Default, "Medicine");
-		//Thêm quyền Approve 
-		medicinePerm.AddChild(
-			SupplyCoreERPPermissions.Catalog.Medicine.Approve,
-			L("Permission:Catalog.Medicine.Approve")
-		);
-	}
+		var medicinePermision = AddCrudPermissions(catalogGroup, SupplyCoreERPPermissions.Catalog.Medicine.Default, "Medicine");
+			// Add Child
+			medicinePermision.AddChild(
+				SupplyCoreERPPermissions.Catalog.Medicine.Approve,
+				L("Permission:Catalog.Medicine.Approve")
+			);
+			medicinePermision.AddChild(
+				SupplyCoreERPPermissions.Catalog.Medicine.Reject,
+				L("Permission:Catalog.Medicine.Reject")
+			);
+        #endregion
 
-	/// <summary>
-	/// Hàm helper tạo bộ quyền chuẩn: Xem, Thêm, Sửa, Xóa
-	/// </summary>
-	/// <returns>Trả về permission cha (Xem) để có thể add thêm child nếu cần</returns>
-	private PermissionDefinition AddCrudPermissions(PermissionGroupDefinition group, string permissionName, string localizationKey)
+        #region Partner Permission
+        // Parent Group
+        var partnerGroup = context.AddGroup(SupplyCoreERPPermissions.Partner.GroupNamePartner, L("Permission:Partner"));
+
+		// Customer
+        AddCrudPermissions(partnerGroup, SupplyCoreERPPermissions.Partner.Customer.Default, "Customer");
+
+		// Supplier
+        AddCrudPermissions(partnerGroup, SupplyCoreERPPermissions.Partner.Supplier.Default, "Supplier");
+        #endregion
+
+        #region Inventory Permission
+        // Parent Group
+        var inventoryGroup = context.AddGroup(SupplyCoreERPPermissions.Inventory.GroupNameInventory, L("Permission:Inventory"));
+
+		// Warehouse
+        var warehousePermission = AddCrudPermissions(inventoryGroup, SupplyCoreERPPermissions.Inventory.Warehouse.Default, "Warehouse");
+			// Add Child
+			warehousePermission.AddChild(
+				SupplyCoreERPPermissions.Inventory.Warehouse.Approve,
+				L("Permission:Inventory.Warehous.Approve")
+			);
+			warehousePermission.AddChild(
+				SupplyCoreERPPermissions.Inventory.Warehouse.Reject,
+				L("Permission:Inventory.Warehous.Reject")
+			);
+
+        // Batch
+        AddCrudPermissions(inventoryGroup, SupplyCoreERPPermissions.Inventory.Warehouse.Default, "Batch");
+
+		// Ticket
+        var ticketPermission = AddCrudPermissions(inventoryGroup, SupplyCoreERPPermissions.Inventory.Warehouse.Default, "Ticket");
+			// Add Child
+			ticketPermission.AddChild(
+					SupplyCoreERPPermissions.Inventory.Ticket.Approve,
+					L("Permission:Inventory.Ticket.Approve")
+			);
+			ticketPermission.AddChild(
+				SupplyCoreERPPermissions.Inventory.Ticket.Reject,
+				L("Permission:Inventory.Ticket.Reject")
+			);
+        #endregion
+
+        #region Order Permission
+        // Parent Group
+        var orderGroup = context.AddGroup(SupplyCoreERPPermissions.Order.GroupNameOrder, L("Permission:Order"));
+
+        // PurchaseOrder
+        var purchaseOrderPermission = AddCrudPermissions(orderGroup, SupplyCoreERPPermissions.Order.PurchaseOrder.Default, "PurchaseOrder");
+			// Add Child
+			purchaseOrderPermission.AddChild(
+					SupplyCoreERPPermissions.Order.PurchaseOrder.Approve,
+					L("Permission:Order.PurchaseOrder.Approve")
+			);
+			purchaseOrderPermission.AddChild(
+				SupplyCoreERPPermissions.Order.PurchaseOrder.Reject,
+				L("Permission:Order.PurchaseOrder.Reject")
+			);
+
+        // SaleOrder
+        var saleOrderPermisson = AddCrudPermissions(orderGroup, SupplyCoreERPPermissions.Order.SaleOrder.Default, "SaleOrder");
+			// Add Child
+			saleOrderPermisson.AddChild(
+					SupplyCoreERPPermissions.Order.SaleOrder.Approve,
+					L("Permission:Order.SaleOrder.Approve")
+			);
+			saleOrderPermisson.AddChild(
+				SupplyCoreERPPermissions.Order.SaleOrder.Reject,
+				L("Permission:Order.SaleOrder.Reject")
+			);
+        #endregion
+
+
+    }
+
+    /// <summary>
+    ///	CRUD Permission Initialization Method (can add child)
+    /// </summary>
+	/// <param>
+	/// group: parent permission
+	/// permissionName: permission key
+	/// localizationKey: localizaion key 
+	/// </param>
+    /// <returns>
+	/// permission child belong parent
+	/// </returns>
+    private PermissionDefinition AddCrudPermissions(PermissionGroupDefinition group, string permissionName, string localizationKey)
 	{
-		//Quyền mặc định (VIEW/XEM)
+		// Default (View) 
 		var parentPermission = group.AddPermission(
 			permissionName,
 			L($"Permission:Catalog.{localizationKey}")
 		);
 
-		//Quyền CREATE
+		// Create
 		parentPermission.AddChild(
 			$"{permissionName}.Create",
 			L($"Permission:Catalog.{localizationKey}.Create")
 		);
 
-		//Quyền UPDATE
+		// Update
 		parentPermission.AddChild(
 			$"{permissionName}.Update",
 			L($"Permission:Catalog.{localizationKey}.Update")
 		);
 
-		//Quyền DELETE
+		// Delete
 		parentPermission.AddChild(
 			$"{permissionName}.Delete",
 			L($"Permission:Catalog.{localizationKey}.Delete")
@@ -68,7 +155,8 @@ public class SupplyCoreERPPermissionDefinitionProvider : PermissionDefinitionPro
 		return parentPermission;
 	}
 
-	private static LocalizableString L(string name)
+    // Localization Mapping
+    private static LocalizableString L(string name)
 	{
 		return LocalizableString.Create<SupplyCoreERPResource>(name);
 	}
--- src/SupplyCoreERP.Application.Contracts/Permissions/SupplyCoreERPPermissions.cs
+++ src/SupplyCoreERP.Application.Contracts/Permissions/SupplyCoreERPPermissions.cs
@@ -2,15 +2,16 @@ namespace SupplyCoreERP.Permissions;
 
 public static class SupplyCoreERPPermissions
 {
+    // Core Permission
 	public const string GroupName = "SupplyCoreERP";
 
-	//Group Catalog
-	public static class Catalog
+    #region Catalog Permission
+    public static class Catalog
 	{
 		public const string GroupNameCatalog = "Catalog";
 
-		//Category
-		public static class Category
+        //Category Permisstion
+        public static class Category
 		{
 			public const string Default = GroupNameCatalog + ".Category"; 
 			public const string Create = Default + ".Create";
@@ -18,18 +19,19 @@ public static class SupplyCoreERPPermissions
 			public const string Delete = Default + ".Delete";
 		}
 
-		//Medicine
-		public static class Medicine
+        //Medicine Permisstion
+        public static class Medicine
 		{
 			public const string Default = GroupNameCatalog + ".Medicine";
 			public const string Create = Default + ".Create";
 			public const string Update = Default + ".Update";
 			public const string Delete = Default + ".Delete";
-			public const string Approve = Default + ".Approve"; 
-		}
+			public const string Approve = Default + ".Approve";
+            public const string Reject = Default + ".Reject";
+        }
 
-		//BaseUnit
-		public static class BaseUnit
+        //BaseUnit Permisstion
+        public static class BaseUnit
 		{
 			public const string Default = GroupNameCatalog + ".BaseUnit";
 			public const string Create = Default + ".Create";
@@ -37,8 +39,8 @@ public static class SupplyCoreERPPermissions
 			public const string Delete = Default + ".Delete";
 		}
 
-		//DosageForm
-		public static class DosageForm
+        //DosageForm Permisstion
+        public static class DosageForm
 		{
 			public const string Default = GroupNameCatalog + ".DosageForm";
 			public const string Create = Default + ".Create";
@@ -46,8 +48,8 @@ public static class SupplyCoreERPPermissions
 			public const string Delete = Default + ".Delete";
 		}
 
-		//ActiveIngredient
-		public static class ActiveIngredient
+        //ActiveIngredient Permisstion
+        public static class ActiveIngredient
 		{
 			public const string Default = GroupNameCatalog + ".ActiveIngredient";
 			public const string Create = Default + ".Create";
@@ -55,8 +57,8 @@ public static class SupplyCoreERPPermissions
 			public const string Delete = Default + ".Delete";
 		}
 
-		//Manufacturer
-		public static class Manufacturer
+        //Manufacturer Permisstion
+        public static class Manufacturer
 		{
 			public const string Default = GroupNameCatalog + ".Manufacturer";
 			public const string Create = Default + ".Create";
@@ -64,4 +66,97 @@ public static class SupplyCoreERPPermissions
 			public const string Delete = Default + ".Delete";
 		}
 	}
+    #endregion
+
+    #region Partner Permission
+	public static class Partner
+	{
+        public const string GroupNamePartner = "Partner";
+
+        // Customer Permisstion
+        public static class Customer
+        {
+            public const string Default = GroupNamePartner + ".Customer";
+            public const string Create = Default + ".Create";
+            public const string Update = Default + ".Update";
+            public const string Delete = Default + ".Delete";
+        }
+
+		// Supplier Permisstion
+        public static class Supplier
+        {
+            public const string Default = GroupNamePartner + ".Supplier";
+            public const string Create = Default + ".Create";
+            public const string Update = Default + ".Update";
+            public const string Delete = Default + ".Delete";
+        }
+    }
+    #endregion
+
+    #region Inventory Permission
+    public static class Inventory
+	{
+		public const string GroupNameInventory = "Inventory";
+
+		// Warehouse Permission
+		public static class Warehouse
+		{
+            public const string Default = GroupNameInventory + ".Warehouse";
+            public const string Create = Default + ".Create";
+            public const string Update = Default + ".Update";
+            public const string Delete = Default + ".Delete";
+            public const string Approve = Default + ".Approve";
+            public const string Reject = Default + ".Reject";
+        }
+
+		// Batch Permission
+		public static class Batch
+		{
+            public const string Default = GroupNameInventory + ".Batch";
+            public const string Create = Default + ".Create";
+            public const string Update = Default + ".Update";
+            public const string Delete = Default + ".Delete";
+        }
+
+        // Ticket Permission
+        public static class Ticket
+        {
+            public const string Default = GroupNameInventory + ".Ticket";
+            public const string Create = Default + ".Create";
+            public const string Update = Default + ".Update";
+            public const string Delete = Default + ".Delete";
+            public const string Approve = Default + ".Approve";
+            public const string Reject = Default + ".Reject";
+        }
+    }
+    #endregion
+
+    #region Order Permission
+    public static class Order
+    {
+        public const string GroupNameOrder = "Order";
+
+        // PurchaseOrder Permission
+        public static class PurchaseOrder
+        {
+            public const string Default = GroupNameOrder + ".PurchaseOrder";
+            public const string Create = Default + ".Create";
+            public const string Update = Default + ".Update";
+            public const string Delete = Default + ".Delete";
+            public const string Approve = Default + ".Approve";
+            public const string Reject = Default + ".Reject";
+        }
+
+        // Sale Order Permission
+        public static class SaleOrder
+        {
+            public const string Default = GroupNameOrder + ".SaleOrder";
+            public const string Create = Default + ".Create";
+            public const string Update = Default + ".Update";
+            public const string Delete = Default + ".Delete";
+            public const string Approve = Default + ".Approve";
+            public const string Reject = Default + ".Reject";
+        }
+    }
+    #endregion
 }
